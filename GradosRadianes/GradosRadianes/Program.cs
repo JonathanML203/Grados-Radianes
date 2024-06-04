34a4e55f7f463bcb82532ca7d73f0a329c13de0c
Jonathan Morales Lopez <74636947+JonathanML203@users.noreply.github.com>
2024-06-04 12:19:03 -0600

    Add files via upload

---
+++ GradosRadianes/GradosRadianes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradosRadianes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Qué tipo de conversión deseas realizar?");
+            Console.WriteLine("1. Grados a Radianes");
+            Console.WriteLine("2. Radianes a Grados");
+
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    ConvertirGradosARadianes();
+                    break;
+                case 2:
+                    ConvertirRadianesAGrados();
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        }
+
+        static void ConvertirGradosARadianes()
+        {
+            Console.Write("Introduce el valor en grados: ");
+            double grados = Convert.ToDouble(Console.ReadLine());
+            double radianes = grados * (Math.PI / 180);
+            Console.WriteLine($"{grados} grados equivalen a {radianes} radianes.");
+        }
+
+        static void ConvertirRadianesAGrados()
+        {
+            Console.Write("Introduce el valor en radianes: ");
+            double radianes = Convert.ToDouble(Console.ReadLine());
+            double grados = radianes * (180 / Math.PI);
+            Console.WriteLine($"{radianes} radianes equivalen a {grados} grados.");
+        }
+    }
+}
